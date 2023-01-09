edd404f932d5d42f8eed6019e947d70c9ee937f4
Rioba-Ian-Felix <riobafelix@gmail.com>
2023-01-09 13:01:08 +0300

    clean html and add comments to game

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ const ROCK_PAPER_SCISSORS = ['Rock', 'Paper', 'Scissors', 'Rock', 'Paper', 'Scis
 game()
 
 function game() {
+    // variable to hold the player Answer
     let playerAnswer
 
+    // get all buttons.
     const btns = document.querySelectorAll("button")
+    // the computer result from the random number of the ROCK_PAPER_SCISSORS
     const randomChoice = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
     const compChoice = ROCK_PAPER_SCISSORS[randomChoice]
     const compResult = document.querySelector(".result").children[1]
 
+    // where we shall store the game result in the ui. 
     const gameResult = document.createElement("h3")
+    // grab the result 
     const resultPlayer = document.querySelector(".result").firstElementChild
 
-
+    // click events for button to launch game once it has been clicked.
     btns.forEach((btn) => btn.addEventListener("click", () => {
         playerAnswer = btn.value
         resultPlayer.textContent = `You have chosen ${playerAnswer}`
         compResult.textContent = `The computer has chosen ${compChoice}`
 
-
+        // game logic
         if (playerAnswer == "Rock") {
             if (compChoice == "Scissors") {
                 gameResult.textContent = "You win, Rock beats Scissors."
@@ -54,13 +59,10 @@ function game() {
                 console.log("This is a draw.");
             }
         }
+        // append the game result after game logic to the ui
         document.querySelector('.result').appendChild(gameResult)
         console.log(playerAnswer);
     }))
 }
 
 
-// function getComputerChoice() {
-//     const randomChoice = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
-//     return ROCK_PAPER_SCISSORS[randomChoice]
-// }
