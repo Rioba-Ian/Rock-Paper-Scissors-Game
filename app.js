bf3ee2d3f8f81f3ec998ca940129e3db29e7bebb
Rioba-Ian-Felix <riobafelix@gmail.com>
2023-01-09 12:39:25 +0300

    ui --html and Js for game

--- app.js
+++ app.js
@@ -1,96 +1,66 @@
 const ROCK_PAPER_SCISSORS = ['Rock', 'Paper', 'Scissors', 'Rock', 'Paper', 'Scissors']
 
-let result = 0
-
-let gameCount = 0
-
-function game() {
-
-    const gameResult = document.querySelector(".result").children[2]
-
-
-    if (gameCount == 0) {
-        gameResult.textContent = result
-        return "not started."
-    } else {
-        gameResult.textContent = result
-        console.log(playRound(getPlayerSelection(), getComputerChoice()));
-    }
-}
-
-
 game()
 
-// getPlayerSelection()
-
-
-
-function getPlayerSelection() {
+function game() {
     let playerAnswer
 
     const btns = document.querySelectorAll("button")
-
+    const randomChoice = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
+    const compChoice = ROCK_PAPER_SCISSORS[randomChoice]
     const compResult = document.querySelector(".result").children[1]
 
-
+    const gameResult = document.createElement("h3")
     const resultPlayer = document.querySelector(".result").firstElementChild
+
 
     btns.forEach((btn) => btn.addEventListener("click", () => {
         playerAnswer = btn.value
         resultPlayer.textContent = `You have chosen ${playerAnswer}`
-        compResult.textContent = `The computer has chosen ${getComputerChoice()}`
-        gameResult += 1
+        compResult.textContent = `The computer has chosen ${compChoice}`
+
+
+        if (playerAnswer == "Rock") {
+            if (compChoice == "Scissors") {
+                gameResult.textContent = "You win, Rock beats Scissors."
+                console.log("You win, Rock beats Scissors.");
+            } else if (compChoice == "Paper") {
+                gameResult.textContent = "You lose, Paper beats Rock"
+                console.log("You lose, Paper beats Rock");
+            } else if (compChoice == "Rock") {
+                gameResult.textContent = "This is a draw."
+                console.log("This is a draw.");
+            }
+        } else if (playerAnswer == "Paper") {
+            if (compChoice == "Scissors") {
+                gameResult.textContent = "You lose, Scissors beats Paper."
+                console.log("You lose, Scissors beats Paper.");
+            } else if (compChoice == "Rock") {
+                gameResult.textContent = "You win, Paper beats Rock"
+                console.log("You win, Paper beats Rock");
+            } else if (compChoice == "Paper") {
+                gameResult.textContent = "This is a draw."
+                console.log("This is a draw.");
+            }
+        } else {
+            if (compChoice == "Rock") {
+                gameResult.textContent = "You lose, Rock beats Scissors."
+                console.log("You lose, Rock beats Scissors.");
+            } else if (compChoice == "Paper") {
+                gameResult.textContent = "You win, Scissors beats Paper"
+                console.log("You win, Scissors beats Paper");
+            } else if (compChoice == "Scissors") {
+                gameResult.textContent = "This is a draw."
+                console.log("This is a draw.");
+            }
+        }
+        document.querySelector('.result').appendChild(gameResult)
         console.log(playerAnswer);
     }))
-
-    return playerAnswer
 }
 
 
-function playRound(playerSelection, computerSelection) {
-
-
-
-    if (playerSelection == computerSelection) {
-        result = result
-        return "This is a draw."
-    }
-
-    else if (playerSelection == "Rock") {
-        if (computerSelection == "Scissors") {
-            result += 1
-            return "You win! Rock beats Scissors."
-
-        }
-        else {
-            result -= 1
-
-            return "You Lose! Paper beats Rock"
-        }
-    }
-
-    else if (playerSelection == "Paper") {
-        if (computerSelection == "Rock") {
-            result += 1
-            return "You win! Paper beats Scissors."
-        } else {
-            result -= 1
-            return "You Lose! Paper beats Rock"
-        }
-    }
-    else {
-        if (computerSelection == "Paper") {
-            result += 1
-            return "You win! Scissors beats Paper."
-        } else {
-            result -= 1
-            return "You lose!Rock beats Scissors."
-        }
-    }
-}
-
-
-function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
-    return ROCK_PAPER_SCISSORS[randomChoice]
-}
+// function getComputerChoice() {
+//     const randomChoice = Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)
+//     return ROCK_PAPER_SCISSORS[randomChoice]
+// }
